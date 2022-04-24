adeef5a2c2464668158b3fe18d4f3f6cd4bd4684
mazarinno <mcsfakianos@gmail.com>
2022-04-24 09:26:57 -0500

    files2

---
+++ Source.cpp
@@ -0,0 +1,428 @@
+﻿//============================================================================
+// Name        : BinarySearchTree.cpp
+// Author      : Mitch Sfakianos
+// Version     : 1.0
+// Copyright   : Copyright Â© 2017 SNHU COCE
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <time.h>
+
+#include "CSVparser.hpp"
+
+using namespace std;
+
+//============================================================================
+// Global definitions visible to all methods and classes
+//============================================================================
+
+// forward declarations
+double strToDouble(string str, char ch);
+
+// define a structure to hold bid information
+struct Course {
+    string courseId; // unique identifier
+    string name;
+    string prereqOne;
+    string prereqTwo;
+};
+
+// Internal structure for tree node
+struct Node {
+    Course course;
+    Node* left;
+    Node* right;
+
+    // Default constructor
+    Node() {
+        left = nullptr;
+        right = nullptr;
+    }
+
+    // constructor w/ bid 
+    Node(Course t_course) : Node() {
+        this->course = t_course;
+    }
+};
+
+//============================================================================
+// Binary Search Tree class definition
+//============================================================================
+
+/**
+ * Define a class containing data members and methods to
+ * implement a binary search tree
+ */
+class BinarySearchTree {
+
+private:
+    Node* root;
+
+    void addNode(Node* node, Course course);
+    void inOrder(Node* node);
+    void postOrder(Node* node);
+    void preOrder(Node* node);
+    Node* removeNode(Node* node, string bidId);
+
+public:
+    BinarySearchTree();
+    virtual ~BinarySearchTree();
+    void InOrder();
+    void PostOrder();
+    void PreOrder();
+    void Insert(Course course);
+    void Remove(string courseId);
+    Course Search(string courseId);
+};
+
+/**
+ * Default constructor
+ */
+BinarySearchTree::BinarySearchTree() {
+    // FixMe (1): initialize housekeeping variables
+    //root is equal to nullptr
+    root = nullptr;
+}
+
+/**
+ * Destructor
+ */
+BinarySearchTree::~BinarySearchTree() {
+    // recurse from root deleting every node
+}
+
+/**
+ * Traverse the tree in order
+ */
+void BinarySearchTree::InOrder() {
+    // FixMe (2): In order root
+    // call inOrder fuction and pass root 
+    this->inOrder(root);
+}
+
+/**
+ * Traverse the tree in post-order
+ */
+void BinarySearchTree::PostOrder() {
+    // FixMe (3): Post order root
+    // postOrder root
+    this->postOrder(root);
+}
+
+/**
+ * Traverse the tree in pre-order
+ */
+void BinarySearchTree::PreOrder() {
+    // FixMe (4): Pre order root
+    // preOrder root
+    this->preOrder(root);
+}
+
+/**
+ * Insert a bid
+ */
+void BinarySearchTree::Insert(Course course) {
+    // FIXME (5) Implement inserting a bid into the tree
+    // if root equal to null ptr
+    if (root == nullptr) {
+        // root is equal to new node bid
+        root = new Node(course);
+    }
+    else {
+        // add Node root and bid
+        this->addNode(root, course);
+    }
+}
+
+/**
+ * Remove a bid
+ */
+void BinarySearchTree::Remove(string bidId) {
+    // FIXME (6) Implement removing a bid from the tree
+    // remove node root bidID
+    this->removeNode(root, bidId);
+}
+
+/**
+ * Search for a bid
+ */
+Course BinarySearchTree::Search(string courseId) {
+    // FIXME (7) Implement searching the tree for a bid
+    // set current node equal to root
+    Node* current = root;
+
+    // keep looping downwards until bottom reached or matching bidId found
+    while (current != nullptr) {
+        // if match found, return current bid
+        if (current->course.courseId == courseId) {
+            return current->course;
+        }
+
+        // if bid is smaller than current node then traverse left
+        if (courseId.compare(current->course.courseId) < 0) {
+            current = current->left;
+        }
+        // else larger so traverse right
+        else {
+            current = current->right;
+        }
+    }
+
+    Course course;
+    return course;
+}
+
+/**
+ * Add a bid to some node (recursive)
+ *
+ * @param node Current node in tree
+ * @param bid Bid to be added
+ */
+void BinarySearchTree::addNode(Node* node, Course course) {
+    // FIXME (8) Implement inserting a bid into the tree    
+    // if node is larger then add to left
+    if (node->course.courseId.compare(course.courseId) > 0) {
+        // if no left node
+        if (node->left == nullptr) {
+            // this node becomes left
+            node->left = new Node(course);
+        }
+        // else recurse down the left node
+        else {
+            this->addNode(node->left, course);
+        }
+    }
+    // else
+    else {
+        // if no right node
+        if (node->right == nullptr) {
+            // this node becomes right
+            node->right = new Node(course);
+        }
+        //else
+        else {
+            // recurse up the right node
+            this->addNode(node->right, course);
+        }
+    }
+
+}
+void BinarySearchTree::inOrder(Node* node) {
+    // FixMe (9): Pre order root
+    //if node is not equal to null ptr
+    if (node != nullptr) {
+        //inOrder left
+        inOrder(node->left);
+        //output bidID, title, amount, fund
+        cout << node->course.courseId << ": " << node->course.name << " | " << node->course.prereqOne << " | " << node->course.prereqTwo << endl;
+        //inOrder right
+        inOrder(node->right);
+    }
+}
+void BinarySearchTree::postOrder(Node* node) {
+    // FixMe (10): Pre order root
+    //if node is not equal to null ptr
+    if (node != nullptr) {
+        //postOrder left
+        postOrder(node->left);
+        //postOrder right
+        postOrder(node->right);
+        //output bidID, title, amount, fund
+        cout << node->course.courseId << ": " << node->course.name << " | " << node->course.prereqOne << " | " << node->course.prereqTwo << endl;
+    }
+}
+
+void BinarySearchTree::preOrder(Node* node) {
+    // FixMe (11): Pre order root
+    //if node is not equal to null ptr
+    if (node != nullptr) {
+        //output bidID, title, amount, fund
+        cout << node->course.courseId << ": " << node->course.name << " | " << node->course.prereqOne << " | " << node->course.prereqTwo << endl;
+        //preOrder left
+        preOrder(node->left);
+        //preOrder right
+        preOrder(node->right);
+    }
+}
+
+Node* BinarySearchTree::removeNode(Node* node, string bidId) {
+    //if node is not equal to null ptr
+    if (node != nullptr) {
+        // if bidid is less than node bidid, recursive  left 
+        if (bidId.compare(node->course.courseId) < 0) {
+            node->left = removeNode(node->left, bidId);
+        }
+        else if (bidId.compare(node->course.courseId) > 0) { // if bidid is more than node bidid, recursive  right
+            node->right = removeNode(node->right, bidId);
+        }
+        else {
+            // if node has no child, delete
+            if (node->left == nullptr && node->right == nullptr) {
+                delete node;
+                node = nullptr;
+            }
+            // if node has one child on left, move node pointer to left child
+            else if (node->left != nullptr && node->right == nullptr) {
+                Node* temp = node;
+                node = node->left;
+                delete temp;
+            }
+            // if node has one child on right, move node pointer to right child
+            else if (node->left == nullptr && node->right != nullptr) {
+                Node* temp = node;
+                node = node->right;
+                delete temp;
+            }
+            // if node has two children
+            else {
+                Node* temp = node->right;
+                while (temp->left != nullptr) {
+                    temp = temp->left;
+                }
+                node->course = temp->course;
+                node->right = removeNode(node->right, temp->course.courseId);
+            }
+        }
+    }
+
+    return node;
+}
+
+//============================================================================
+// Static methods used for testing
+//============================================================================
+
+/**
+ * Display the bid information to the console (std::out)
+ *
+ * @param bid struct containing the bid info
+ */
+void displayCourse(Course course) {
+    cout << course.courseId << ": " << course.name << " | " << course.prereqOne << " | "
+        << course.prereqTwo << endl;
+    return;
+}
+
+/**
+ * Load a CSV file containing bids into a container
+ *
+ * @param csvPath the path to the CSV file to load
+ * @return a container holding all the bids read
+ */
+void loadCourses(string csvPath, BinarySearchTree* bst) {
+    cout << "Loading CSV file " << csvPath << endl;
+
+    // initialize the CSV Parser using the given path
+    csv::Parser file = csv::Parser(csvPath);
+
+    // read and display header row - optional
+    vector<string> header = file.getHeader();
+    for (auto const& c : header) {
+        cout << c << " | ";
+    }
+    cout << "" << endl;
+
+    try {
+        // loop to read rows of a CSV file
+        for (unsigned int i = 0; i < file.rowCount(); i++) {
+
+            // Create a data structure and add to the collection of bids
+            Course course;
+            course.courseId = file[i][1];
+            course.name = file[i][0];
+            course.prereqOne = file[i][8];
+            course.prereqTwo = file[i][8];
+
+            //cout << "Item: " << bid.title << ", Fund: " << bid.fund << ", Amount: " << bid.amount << endl;
+
+            // push this bid to the end
+            bst->Insert(course);
+        }
+    }
+    catch (csv::Error& e) {
+        std::cerr << e.what() << std::endl;
+    }
+}
+
+/**
+ * The one and only main() method
+ */
+int main(int argc, char* argv[]) {
+
+    // process command line arguments
+    string csvPath, bidKey;
+    switch (argc) {
+    default:
+        csvPath = "courseList.csv";
+        bidKey = "98109";
+    }
+
+    // Define a timer variable
+    clock_t ticks;
+
+    // Define a binary search tree to hold all bids
+    BinarySearchTree* bst;
+
+    Course course;
+
+    int choice = 0;
+    while (choice != 9) {
+        cout << "Menu:" << endl;
+        cout << "  1. Load Courses" << endl;
+        cout << "  2. Display All Courses" << endl;
+        cout << "  3. Find Course" << endl;
+        cout << "  9. Exit" << endl;
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        bst = new BinarySearchTree();
+
+        switch (choice) {
+
+        case 1:
+            bst = new BinarySearchTree();
+
+            // Initialize a timer variable before loading bids
+            ticks = clock();
+
+            // Complete the method call to load the bids
+            loadCourses(csvPath, bst);
+
+            //cout << bst->Size() << " bids read" << endl;
+
+            // Calculate elapsed time and display result
+            ticks = clock() - ticks; // current clock ticks minus starting clock ticks
+            cout << "time: " << ticks << " clock ticks" << endl;
+            cout << "time: " << ticks * 1.0 / CLOCKS_PER_SEC << " seconds" << endl;
+            break;
+
+        case 2:
+            bst->InOrder();
+            break;
+
+        case 3:
+            ticks = clock();
+
+            course = bst->Search(bidKey);
+
+            ticks = clock() - ticks; // current clock ticks minus starting clock ticks
+
+            if (!course.courseId.empty()) {
+                displayCourse(course);
+            }
+            else {
+                cout << "Bid Id " << bidKey << " not found." << endl;
+            }
+
+            cout << "time: " << ticks << " clock ticks" << endl;
+            cout << "time: " << ticks * 1.0 / CLOCKS_PER_SEC << " seconds" << endl;
+
+            break;
+        }
+    }
+
+    cout << "Good bye." << endl;
+
+    return 0;
+}
